2bf372206cec96fdb59fe62380c4e26e1fbade6a
MGDArrow <PSMuz@mgdmail.ru>
2025-08-03 20:25:08 +0300

    Git init & v.1.1.0

---
+++ index.js
@@ -0,0 +1,21 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  trailingComma: "all",
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  vueIndentScriptAndStyle: true,
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+  htmlSelfClosing: true,
+  htmlWhitespaceSensitivity: "css"
+}
+
+export default config;
